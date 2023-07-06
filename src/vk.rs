11a8e9b3da8ab4d8adbc11dbd3f543b39c96d3f6
Victor Polevoy <fx@thefx.co>
2023-07-06 14:46:40 +0200

    Add debug printing of feature parameters

--- src/vk.rs
+++ src/vk.rs
@@ -207,6 +207,35 @@ impl System {
     fn shutdown(&self) -> Result {
         unsafe { bindings::NVSDK_NGX_VULKAN_Shutdown1(self.device.to_pointer_mut()) }.into()
     }
+
+    /// Creates a new [`Feature`] with the logical device used to create
+    /// this [`System`].
+    pub fn create_feature(
+        &self,
+        command_buffer: vk::CommandBuffer,
+        feature_type: bindings::NVSDK_NGX_Feature,
+        parameters: Option<FeatureParameters>,
+    ) -> Result<Feature> {
+        let parameters = match parameters {
+            Some(p) => p,
+            None => FeatureParameters::get_capability_parameters()?,
+        };
+        Feature::new(self.device, command_buffer, feature_type, parameters)
+    }
+
+    /// Creates a supersampling feature.
+    pub fn create_supersampling_feature(
+        &self,
+        command_buffer: vk::CommandBuffer,
+        parameters: Option<FeatureParameters>,
+    ) -> Result<SuperSamplingFeature> {
+        self.create_feature(
+            command_buffer,
+            bindings::NVSDK_NGX_Feature::NVSDK_NGX_Feature_SuperSampling,
+            parameters,
+        )
+        .map(|f| unsafe { SuperSamplingFeature::from_feature_unchecked(f) })
+    }
 }
 
 impl Drop for System {
@@ -244,9 +273,142 @@ impl Drop for FeatureHandle {
 // pub type FeatureParameterName = std::ffi::CStr;
 pub type FeatureParameterName = [u8];
 
+macro_rules! insert_parameter_debug {
+    ($map:ident, $parameters:ident, ($key:path, bool),) => {
+        if let Ok(value) = $parameters.get_bool($key) {
+            $map.insert(
+                stringify!($key).to_owned(),
+                format!("{:?}", value)
+                );
+        }
+    };
+    ($map:ident, $parameters:ident, ($key:path, i32),) => {
+        if let Ok(value) = $parameters.get_i32($key) {
+            $map.insert(
+                stringify!($key).to_owned(),
+                format!("{:?}", value),
+            );
+        }
+    };
+    ($map:ident, $parameters:ident, ($key:path, u32),) => {
+        if let Ok(value) = $parameters.get_u32($key) {
+            $map.insert(
+                stringify!($key).to_owned(),
+                format!("{:?}", value),
+            );
+        }
+    };
+    ($map:ident, $parameters:ident, ($key:path, f32),) => {
+        if let Ok(value) = $parameters.get_f32($key) {
+            $map.insert(
+                stringify!($key).to_owned(),
+                format!("{:?}", value),
+            );
+        }
+    };
+    ($map:ident, $parameters:ident, ($key:path, u64),) => {
+        if let Ok(value) = $parameters.get_u64($key) {
+            $map.insert(
+                stringify!($key).to_owned(),
+                format!("{:?}", value),
+            );
+        }
+    };
+    ($map:ident, $parameters:ident, ($key:path, f64),) => {
+        if let Ok(value) = $parameters.get_f64($key) {
+            $map.insert(
+                stringify!($key).to_owned(),
+                format!("{:?}", value),
+            );
+        }
+    };
+    ($map:ident, $parameters:ident, ($key:path, $typ:ident), $(($next_key:path, $next_type:ident)),+,) => {
+        insert_parameter_debug!($map, $parameters, ($key, $typ),);
+        insert_parameter_debug!($map, $parameters, $(($next_key, $next_type)),+,);
+    };
+}
+
 /// Feature parameters is a collection of parameters of a feature (ha!).
-#[derive(Debug)]
+// #[derive(Debug)]
 pub struct FeatureParameters(*mut bindings::NVSDK_NGX_Parameter);
+
+impl std::fmt::Debug for FeatureParameters {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        struct FeatureParametersDebugPrinter<'a>(&'a FeatureParameters);
+
+        impl<'a> std::fmt::Debug for FeatureParametersDebugPrinter<'a> {
+            fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+                use std::collections::HashMap;
+
+                let mut fmt = fmt.debug_struct("FeatureParameters");
+                fmt.field("pointer_address", &self.0 .0);
+
+                let populate_map = || -> HashMap<String, String> {
+                    let mut map = HashMap::new();
+                    let parameters = self.0;
+
+                    // TODO: add more
+                    insert_parameter_debug!(
+                        map,
+                        parameters,
+                        (
+                            crate::bindings::NVSDK_NGX_Parameter_SuperSampling_Available,
+                            bool
+                        ),
+                        (
+                            crate::bindings::NVSDK_NGX_Parameter_InPainting_Available,
+                            bool
+                        ),
+                        (
+                            crate::bindings::NVSDK_NGX_Parameter_ImageSuperResolution_Available,
+                            bool
+                        ),
+                        (
+                            crate::bindings::NVSDK_NGX_Parameter_SlowMotion_Available,
+                            bool
+                        ),
+                        (
+                            crate::bindings::NVSDK_NGX_Parameter_VideoSuperResolution_Available,
+                            bool
+                        ),
+                        (
+                            crate::bindings::NVSDK_NGX_Parameter_ImageSignalProcessing_Available,
+                            bool
+                        ),
+                        (crate::bindings::NVSDK_NGX_Parameter_DeepResolve_Available, bool),
+                        (crate::bindings::NVSDK_NGX_Parameter_DeepDVC_Available, bool),
+                        (crate::bindings::NVSDK_NGX_Parameter_SuperSampling_NeedsUpdatedDriver, bool),
+                        (crate::bindings::NVSDK_NGX_Parameter_InPainting_NeedsUpdatedDriver, bool),
+                        (crate::bindings::NVSDK_NGX_Parameter_ImageSuperResolution_NeedsUpdatedDriver, bool),
+                        (crate::bindings::NVSDK_NGX_Parameter_SlowMotion_NeedsUpdatedDriver, bool),
+                        (crate::bindings::NVSDK_NGX_Parameter_VideoSuperResolution_NeedsUpdatedDriver, bool),
+                        (crate::bindings::NVSDK_NGX_Parameter_ImageSignalProcessing_NeedsUpdatedDriver, bool),
+                        (crate::bindings::NVSDK_NGX_Parameter_DeepResolve_NeedsUpdatedDriver, bool),
+                        (crate::bindings::NVSDK_NGX_Parameter_DeepDVC_NeedsUpdatedDriver, bool),
+                        (crate::bindings::NVSDK_NGX_Parameter_FrameInterpolation_NeedsUpdatedDriver, bool),
+                        (crate::bindings::NVSDK_NGX_Parameter_NumFrames, u32),
+                        (crate::bindings::NVSDK_NGX_Parameter_Scale, u32),
+                        (crate::bindings::NVSDK_NGX_Parameter_OptLevel, u32),
+                        (
+                            crate::bindings::NVSDK_NGX_Parameter_IsDevSnippetBranch,
+                            bool
+                        ),
+                        (
+                            crate::bindings::NVSDK_NGX_Parameter_SuperSampling_ScaleFactor,
+                            f32
+                        ),
+                    );
+                    map
+                };
+                let map = populate_map();
+                fmt.field("parameters", &map).finish()
+            }
+        }
+
+        let debug = FeatureParametersDebugPrinter(self);
+        fmt.debug_tuple("FeatureParameters").field(&debug).finish()
+    }
+}
 
 impl FeatureParameters {
     /// Create a new feature parameter set.
@@ -333,6 +495,32 @@ impl FeatureParameters {
         .map(|_| ptr)
     }
 
+    /// Sets an [bool] value for the parameter named `name`. The
+    /// [bool] type isn't supported in NGX, but the semantics - are. The
+    /// boolean values are stored as integers with value `1` being
+    /// `true` and `0` being `false`.
+    pub fn set_bool(&self, name: &FeatureParameterName, value: bool) {
+        unsafe {
+            bindings::NVSDK_NGX_Parameter_SetI(
+                self.0,
+                name.as_ptr().cast(),
+                if value { 1 } else { 0 },
+            )
+        }
+    }
+
+    /// Returns a [bool] value of a parameter named `name`.
+    /// The [bool] type isn't supported in NGX, but the semantics - are.
+    /// The boolean values are stored as integers with value `1` being
+    /// `true` and `0` being `false`.
+    pub fn get_bool(&self, name: &FeatureParameterName) -> Result<bool> {
+        let mut value = 0i32;
+        Result::from(unsafe {
+            bindings::NVSDK_NGX_Parameter_GetI(self.0, name.as_ptr().cast(), &mut value as *mut _)
+        })
+        .map(|_| if value == 1 { true } else { false })
+    }
+
     /// Sets an [f32] value for the parameter named `name`.
     pub fn set_f32(&self, name: &FeatureParameterName, value: f32) {
         unsafe { bindings::NVSDK_NGX_Parameter_SetF(self.0, name.as_ptr().cast(), value) }
@@ -401,6 +589,31 @@ impl FeatureParameters {
             bindings::NVSDK_NGX_Parameter_GetULL(self.0, name.as_ptr().cast(), &mut value as *mut _)
         })
         .map(|_| value)
+    }
+
+    /// Returns `Ok` if the parameters claim to support the
+    /// super sampling feature ([`bindings::NVSDK_NGX_Parameter_SuperSampling_Available`]).
+    pub fn supports_super_sampling(&self) -> Result<()> {
+        if self.get_bool(bindings::NVSDK_NGX_Parameter_SuperSampling_NeedsUpdatedDriver)? {
+            let major =
+                self.get_u32(bindings::NVSDK_NGX_Parameter_SuperSampling_MinDriverVersionMajor)?;
+            let minor =
+                self.get_u32(bindings::NVSDK_NGX_Parameter_SuperSampling_MinDriverVersionMinor)?;
+            return Err(crate::Error::Other(format!("The SuperSampling feature requires a driver update. The driver version required should be higher or equal to {major}.{minor}")));
+        }
+        match self.get_bool(bindings::NVSDK_NGX_Parameter_SuperSampling_Available) {
+            Ok(true) => Ok(()),
+            Ok(false) => Err(crate::Error::Other(format!(
+                "The SuperSampling isn't supported on this platform."
+            ))),
+            Err(e) => Err(e),
+        }
+    }
+
+    /// Returns `Ok` if the parameters claim to support the
+    /// super sampling feature ([`bindings::NVSDK_NGX_Parameter_SuperSampling_Available`]).
+    pub fn supports_super_sampling_static() -> Result<()> {
+        Self::get_capability_parameters()?.supports_super_sampling()
     }
 
     /// Deallocates the feature parameter set.
@@ -513,6 +726,53 @@ impl Feature {
 #[derive(Debug)]
 pub struct FeatureRequirement(bindings::NVSDK_NGX_FeatureRequirement);
 
+/// A helpful type alias to quickly mention "DLSS".
+pub type DlssFeature = SuperSamplingFeature;
+
+/// A SuperSamling (or "DLSS") feature.
+#[derive(Debug)]
+pub struct SuperSamplingFeature(Feature);
+
+impl SuperSamplingFeature {
+    // /// Attempts to create the [`SuperSamplingFeature`] with the default
+    // /// settings preset.
+    // pub fn try_default() -> Result<Self> {
+    //     let parameters = FeatureParameters::get_capability_parameters()?;
+    //     Self::new(parameters)
+    // }
+
+    // /// Creates a new [`SuperSamplingFeature`] with the given
+    // /// parameters.
+    // pub fn from_feature_unchecked(parameters: FeatureParameters) -> Result<Self> {}
+
+    /// Creates a new [`SuperSamplingFeature`] from the given
+    /// [`Feature`] object.
+    ///
+    /// # Safety
+    ///
+    /// The function is unsafe due to not checking the actual feature
+    /// type within the [`Feature`] provided.
+    ///
+    /// For the safe and checked version use [`std::convert::TryFrom`].
+    pub unsafe fn from_feature_unchecked(feature: Feature) -> Self {
+        Self(feature)
+    }
+}
+
+impl TryFrom<Feature> for SuperSamplingFeature {
+    type Error = crate::Error;
+
+    fn try_from(feature: Feature) -> Result<Self> {
+        if feature.feature_type == bindings::NVSDK_NGX_Feature::NVSDK_NGX_Feature_SuperSampling {
+            Ok(unsafe { Self::from_feature_unchecked(feature) })
+        } else {
+            Err(crate::Error::Other(format!(
+                "The provided Feature isn't of SuperSampling type."
+            )))
+        }
+    }
+}
+
 // #[derive(Debug)]
 // pub struct FeatureCommonInfo {
 //     path_list_info:,
@@ -561,13 +821,43 @@ pub struct FeatureRequirement(bindings::NVSDK_NGX_FeatureRequirement);
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
     #[test]
     fn features() {
+        // TODO: initialise vulkan and be able to do this.
         // dbg!(super::FeatureParameters::get_capability_parameters().unwrap());
     }
 
     #[test]
     fn get_required_extensions() {
         assert!(super::RequiredExtensions::get().is_ok());
+    }
+
+    /// Ignored as it just needs to compile.
+    #[test]
+    #[ignore]
+    fn insert_parameter_debug_macro() -> super::Result {
+        let mut map = HashMap::new();
+        let parameters = super::FeatureParameters::get_capability_parameters().unwrap();
+        insert_parameter_debug!(
+            map,
+            parameters,
+            (crate::bindings::NVSDK_NGX_EParameter_Reserved00, i32),
+            (
+                crate::bindings::NVSDK_NGX_EParameter_SuperSampling_Available,
+                bool
+            ),
+            (
+                crate::bindings::NVSDK_NGX_EParameter_InPainting_Available,
+                bool
+            ),
+            (
+                crate::bindings::NVSDK_NGX_EParameter_ImageSuperResolution_Available,
+                bool
+            ),
+        );
+
+        Ok(())
     }
 }
